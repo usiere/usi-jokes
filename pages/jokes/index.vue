f41ccf46fc893e76f60b776fd2ce78a55e412f7f
usiere <akpabio98@gmail.com>
2022-09-11 11:24:04 +0100

    feat: :fire: Search functionality implemented for Jokes

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <SearchJokes v-on:search-text="searchText" />
        <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke"
         />
     </div>
@@ -9,10 +10,12 @@
 <script>
 import axios from 'axios';
 import Joke from '../../components/Joke';
+import SearchJokes from '../../components/SearchJokes';
 
 export default {
     components: {
-        Joke
+        Joke,
+        SearchJokes
     },
     data(){
         return{
@@ -37,6 +40,26 @@ export default {
 
      
     },
+
+    methods:{
+        async searchText(text) {
+                  const config = {
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+
+        try {
+               const res = await axios.get(`https://icanhazdadjoke.com/search?term=${text}`, config);
+               
+               this.jokes = res.data.results;
+        }
+        catch (err) {
+            console.log(err)
+        }
+        }
+    },
+
     head(){
         return{
             title: 'Usi Jokes',
